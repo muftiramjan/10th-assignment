cac9bb466013cef467ba17aa843ee938894da119
mufti ramjan <alibinalaluddin@gmail.com>
2024-04-30 12:11:18 +0600

    revel animation adeded

--- src/Paigase/Hoome/Hoome.jsx
+++ src/Paigase/Hoome/Hoome.jsx
@@ -1,20 +1,21 @@
 import Slider from "../../components/Slider";
 import Books from "../../components/Book/Books";
 import ResidentialPara from "../../components/ResidentialPara";
-import { Helmet } from "react-helmet-async";
+import { Helmet } from "react-helmet-async"
 
-
-
-  
+import { Fade } from "react-awesome-reveal";
 
 
 const Hoome = () => {
+  
     return (
         <div>
+            <Fade>
+  <p>I will gently appear as I enter the viewport</p>
+</Fade>
             <Helmet>
                 <title>Home</title>
             </Helmet>
-          
             <Slider></Slider>
             <ResidentialPara></ResidentialPara>
             <Books></Books>
